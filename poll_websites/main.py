8899be48e5293fe7fa420ca2857db0a6559003a8
Mark <wernsdorfer@gmail.com>
2019-02-16 11:33:05 +0100

    increased interval to 5 minutes

--- poll_websites/main.py
+++ poll_websites/main.py
@@ -52,7 +52,7 @@ def repeat_indefinitely(function: Callable[[], None], minutes_interval: int):
 
 
 def main():
-    repeat_indefinitely(check_for_changes, 1)
+    repeat_indefinitely(check_for_changes, 5)
 
 
 if __name__ == "__main__":
